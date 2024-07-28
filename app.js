cf7e497bd63c538c7311acd1f966ac2ee071f6e1
Abhinay-bir-17 <abhinaybirsingh10@gmail.com>
2024-07-28 17:32:37 +0530

    1.3 added css file to change color of heading, made seperate file i.e app.js for react code

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading =  React.createElement('h1', {id:'heading'}, 'hello abhinay bir from react which is in app.js')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
