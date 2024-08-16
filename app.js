f7d7014836ebdc91cfdaa6cd9b0d2cb85cbe51e9
Abhinay-bir-17 <abhinaybirsingh10@gmail.com>
2024-08-16 13:41:40 +0530

    added a new script in package.json, used jsx to inject a header in div

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-const heading =  React.createElement('h1', {id:'heading'}, 'hello abhinay bir from react which is in app.js')
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const heading = React.createElement('h1',{},'hello from abhinay bir');
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading)
+console.log('heading:',heading)
 
-/*
-  div id=parent
-    div id=child1
-      h1
-      h2
-    div
-    div id=child2
-      h1
-      h2
-    div
-  div
-*/
-const parent = React.createElement('div',{id:'parent'},[
-    React.createElement('div',{id:'child1'},[
-      React.createElement('h1',{},'h1 header in child1'),
-      React.createElement('h2',{},'h2 header in child1')
-    ])
-],
-    React.createElement('div',{id:'child2'},[
-      React.createElement('h1',{},'h1 header in child2'),
-      React.createElement('h2',{},'h2 header in child2')
-    ])
-)
-root.render(parent)
+// jsx
+const jsxheading = <h1 id='jheading'>hello from jsx</h1>
+const root2 = ReactDOM.createRoot(document.getElementById('root2'))
+console.log('jsheading:', jsxheading)
+root2.render(jsxheading)
+
+
+// const heading =  React.createElement('h1', {id:'heading'}, 'hello abhinay bir from react which is in app.js')
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(heading)
+
+// /*
+//   div id=parent
+//     div id=child1
+//       h1
+//       h2
+//     div
+//     div id=child2
+//       h1
+//       h2
+//     div
+//   div
+// */
+// const parent = React.createElement('div',{id:'parent'},[
+//     React.createElement('div',{id:'child1'},[
+//       React.createElement('h1',{},'h1 header in child1'),
+//       React.createElement('h2',{},'h2 header in child1')
+//     ])
+// ],
+//     React.createElement('div',{id:'child2'},[
+//       React.createElement('h1',{},'h1 header in child2'),
+//       React.createElement('h2',{},'h2 header in child2')
+//     ])
+// )
+// root.render(parent)
+
